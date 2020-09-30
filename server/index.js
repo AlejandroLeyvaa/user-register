3353c3211ce1d35814cfed538e42889f48230d21
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-09-30 07:57:52 -0600

    Auth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const app = express();
 
 const { config } = require('./config/index');
 
-const app = express();
+const authApi = require('./routes/auth');
 
+// body parser
 app.use(express.json());
+
+// routes
+authApi(app);
 
 app.listen(config.port, () => {
   console.log(`Listen on http://localhost:${config.port}`);
